f664db8c4676a33ef3ca51131110b3af15e3088c
ahmad-hawchar <hawchar.business@gmail.com>
2024-01-24 22:17:56 +0200

    added 2 fetching functions for getting users in services folder

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,20 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-
+  backendUrl: string = "https://reqres.in/api/users";
   constructor(private http: HttpClient) { }
-  fetchUsers() {
-    this.http.get("").subscribe{
-      next: {
-
-      },
-      error: {
-
-      }
-    }
+  fetchUsers(page: number = 1) {
+    this.http.get(`${this.backendUrl}?page=${page}`).pipe(
+      map((res) => {
+        return res;
+      }));
+  }
+  fetchOneUser(userId: string) {
+    this.http.get(`${this.backendUrl}/${userId}`).pipe(
+      map((res) => {
+        return res;
+      }));
   }
 }
