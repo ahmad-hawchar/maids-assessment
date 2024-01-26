93edad774e254abb2615e5b2800d14faa0715908
ahmad-hawchar <hawchar.business@gmail.com>
2024-01-26 11:49:16 +0200

    fixed applicationBootsrap errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-bootstrapApplication(AppComponent, appConfig)
+import { provideStore } from '@ngrx/store';
+bootstrapApplication(AppComponent, {
+  providers: [provideStore(), ...appConfig.providers]
+})
   .catch((err) => console.error(err));
